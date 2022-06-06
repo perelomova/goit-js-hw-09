b0ab0c06c06db866af38bf2700fe76866b62b1e1
Kateryna Perelomova <e.v.perelomova@gmail.com>
2022-06-06 20:30:52 +0200

    Update 02-timer.js
    Disabled input when timer start working

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,6 +3,7 @@ import "flatpickr/dist/flatpickr.min.css";
 import Notiflix from 'notiflix';
 
 const refs = {
+  input: document.querySelector('#datetime-picker'),
   startBtn: document.querySelector('[data-start]'),
   days: document.querySelector('[data-days]'),
   hours: document.querySelector('[data-hours]'),
@@ -85,6 +86,7 @@ function convertMs(ms) {
 }
 
 const startTimer = () => {
+  refs.input.disabled = true;
   timerId = setInterval(getTimerValues, 1000);
   refs.startBtn.textContent = 'Stop';
 }
